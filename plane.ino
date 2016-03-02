db644ca51fcaec2d16eabc0bfc26cbd85ffecc98
jjollivet <jeremiejollivet@gmail.com>
2016-03-01 20:26:05 -0500

    Removed a bunch of stuff
    Removed anything to do with airspeed, camera servos, PID.

--- plane.ino
+++ plane.ino
@@ -10,19 +10,8 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 
 //hardware includes
-#include "AirspeedSensor.h"
 #include "Communicator.h"
-#include "DX6.h"
 #include "EagleTreeAltimeterV4.h"
-
-//PWM interupt stuff
-#define NUM_PWM 4
-int PWM_pin[NUM_PWM] = {AILERON_INPUT_PIN, ELEVATOR_INPUT_PIN, PID_MODE_PIN, AILERON_MODE_PIN}; //roll, pitch, PID mode, flap mode
-//array of data sturctures (on for each pin)
-volatile static pinTimingData pinData[NUM_PWM];
-
-//define sensors and measurment classes
-DX6 RC_receiver;
 
 //ROLL PITCH AND YAW CONTROLL
 //SDA pin 4
@@ -50,10 +39,7 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-//variables used by PIDs
-double target_pitch, target_roll;
 double current_pitch, current_roll;
-double offset_pitch, offset_roll;
 double base_pitch, base_roll;
 
 //roll and pitch filters - these are used only to zero the sensor, not for PIDs!
@@ -61,21 +47,12 @@ double base_pitch, base_roll;
 IIR_doubleFilter *roll_filter = new IIR_doubleFilter(0.1);
 IIR_doubleFilter *pitch_filter = new IIR_doubleFilter(0.1);
 
-//XXX Calibrate - DONE!
-int elevator_base_val = 1450;
-int right_aileron_base_val = 1500;
-int left_aileron_base_val = 1500;
-
-//arduino-controlled servos
-Servo right_aileron_servo, left_aileron_servo, elevator_servo;
-
 //system variables
 double altitude;
 double altitude_at_drop;
-double airspeed;
 byte blinkState;
 
-//Tail wheel declarations
+//Servo declarations
 Servo wheel_servo;
 volatile int pwm_value[ ] = {0,0};
 volatile int prev_time[ ] = {0,0};
@@ -87,11 +64,11 @@ volatile int prev_time[ ] = {0,0};
 Communicator comm;
 
 //sensor declerations
-AirspeedSensor airspeedSensor;
 EagleTreeAltimeterV4 altimeter;
 
 unsigned long current_time;
 unsigned long prev_slow_time, prev_medium_time, prev_fast_time, prev_long_time;
+
 //interupt routine
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -108,7 +85,6 @@ void setup(){
   comm.sendMessage(MESSAGE_START);
 
   //attach servos that are controlled directly by communicator
-  comm.attachCamera(CAMERA_PAN_PIN, CAMERA_TILT_PIN);
   comm.attachDropBay(DROP_PIN);
 
   //initialize Data Acquisition System (which also preforms a DAS reset)
@@ -128,9 +104,7 @@ void setup(){
   prev_slow_time = prev_fast_time;
   prev_long_time = prev_fast_time;
   
-  initializeDX6();
-  
-  //send message to gournd station saying everything is ready
+  //send message to ground station saying everything is ready
   comm.sendMessage(MESSAGE_READY);
 }
 
@@ -216,23 +190,9 @@ void fastLoop(){
         }
 
     }    
-
-  //update servo positions
-  //run PIDs
-  //read DX6 for new target pitch and roll
-  RC_receiver.readPWM();
-  target_roll = (int) RC_receiver.getRoll();  //DX6's roll is opposite to MPU6050
-  target_pitch = (int) RC_receiver.getPitch();
-  
-  
-  //unsigned long t2 = micros();  //used for testing timing
-  //Serial.print("  ");
-  //Serial.println(t2 - t1);
-  
 }
 
 void mediumLoop(){
-  updateServos();
   
   //De-mixing of signal for tail wheel
   int signal_wheel;
@@ -250,7 +210,6 @@ void mediumLoop(){
 void slowLoop(){
   //unsigned long t1 = micros(); //for testing timing
  //get latest telementary data 
-  airspeed = airspeedSensor.readAirSpeed(); 
   altitude = altimeter.readAltitude();
   
    //pass new data to serial communicator
@@ -258,7 +217,6 @@ void slowLoop(){
   comm.roll = roll_filter->getCurrentValue() - base_roll;
   comm.pitch = pitch_filter->getCurrentValue() - base_pitch;
   comm.altitude = altitude;
-  comm.airspeed = airspeed;   
   
   //send the next set of data
   //only one variable is sent with each call - 4 calls necessary to send everything
@@ -287,21 +245,8 @@ void slowLoop(){
     
        comm.sendMessage(MESSAGE_RESTART_AKN);
   } 
-  while(comm.calibration_flag){
-      //write latest base values to servos
-      elevator_base_val = comm.ElevatorVal_cal;
-      right_aileron_base_val = comm.RightAileronVal_cal;
-      left_aileron_base_val = comm.LeftAileronVal_cal;
-      
-      elevator_servo.writeMicroseconds(elevator_base_val);
-      right_aileron_servo.writeMicroseconds(right_aileron_base_val);
-      left_aileron_servo.writeMicroseconds(left_aileron_base_val);  
-      
+  while(comm.calibration_flag){     
       comm.calibrate();
-      //get new values
-      elevator_base_val = comm.ElevatorVal_cal;
-      right_aileron_base_val = comm.RightAileronVal_cal;
-      left_aileron_base_val = comm.LeftAileronVal_cal;
   }
   /*
   unsigned long t2 = micros(); //for testing timing
@@ -346,10 +291,6 @@ int tail_wheel_demixing(){
 
 //initialize servo locations
 void initializeServos(){
-  right_aileron_servo.attach(RIGHT_AILERON_SERVO_PIN);
-  left_aileron_servo.attach(LEFT_AILERON_SERVO_PIN);
-  elevator_servo.attach(ELEVATOR_SERVO_PIN);
-  
   //De-mixing of signal to tail wheel
   pinMode(ELEVATOR_INPUT_PIN, INPUT); 
   pinMode(RUDDER_INPUT_PIN, INPUT); 
@@ -360,35 +301,9 @@ void initializeServos(){
   wheel_servo.attach(TAIL_WHEEL_PIN);
 }
 
-//function to update servo positions
-void updateServos(){
-  
-  //XXX Calibrate - DONE!
-  if(RC_receiver.getPIDmode() == 2){ //PID_MODE_ON
-    
-  }
-  else{
-      //PID mode off
-      //check flap mode
-      if(RC_receiver.getAileronMode() == AILERON_NORMAL_MODE){
-          right_aileron_servo.writeMicroseconds(right_aileron_base_val + target_roll); //calibrate +/-
-          left_aileron_servo.writeMicroseconds(left_aileron_base_val - target_roll);  //calibrate +/-
-      }
-      else{
-          right_aileron_servo.writeMicroseconds(right_aileron_base_val + target_roll - RIGHT_AILERON_FLAP_VAL);  //calibrate +/-
-          left_aileron_servo.writeMicroseconds(left_aileron_base_val - target_roll - LEFT_AILERON_FLAP_VAL);    //calibrate +/-    
-      }
-      elevator_servo.writeMicroseconds(elevator_base_val - target_pitch);
-    
-  }
-  
-}
-
 void initializeDAS(){
   Wire.begin();
   //initialize sensors
-  pinMode(AIRSPEED_SENSOR_PIN, INPUT);
-  airspeedSensor.initialize();  
   
   initializeMPU6050();
   
@@ -411,7 +326,6 @@ void resetDAS(){
   
   //call individual reset functions for all the sensors
   altimeter.zero();
-  airspeedSensor.reset();
   
   base_pitch = pitch_filter->getCurrentValue();
   base_roll = roll_filter->getCurrentValue();
@@ -486,42 +400,6 @@ void initializeMPU6050(){
           //Serial.println(F(")"));
       }
 }
-
-//function to initialize the DX6
-void initializeDX6(){
-         //initialize interupt stuff first 
-        //start with pin 0
-
-         //initialize data structure for each pin
-        for(int i = 0; i < NUM_PWM; i++){
-              pinData[i].riseTime = 0;
-              pinData[i].fallTime = 0;
-              pinData[i].lastGoodWidth = 0;
-        }
-        
-
-	//define and calibrate DX6 controler
-        //                     Roll                        Pitch                      PID Mode                  Aileron Flap Mode
-	RC_receiver.initialize(&pinData[0].lastGoodWidth, &pinData[1].lastGoodWidth, &pinData[2].lastGoodWidth, &pinData[3].lastGoodWidth);
-
-        //XXX Calibrate - DONE!
-        //set min, zero, max readings for roll and pitch
-	RC_receiver.calibrateRoll(1099,1494,1917);
-	RC_receiver.calibratePitch(1096,1501,1918);
-
-        //XXX Calibrate - DONE!
-        //set the input roll and pitch ranges
-        //these values reflect the maximum allowed roll and pich of the aircraft
-	RC_receiver.setRollRange(-500, 500); //calibrate - these are servo angles in PWM
-	RC_receiver.setPitchRange(-500, 500);//calibrate - these are servo angles in PWM
-  
-      //start interupt for first pin
-      attachInterrupt(PWM_pin[0], PWM_RISING_ISR_0, RISING); 
-      attachInterrupt(PWM_pin[1], PWM_RISING_ISR_1, RISING);  
-      attachInterrupt(PWM_pin[2], PWM_RISING_ISR_2, RISING); 
-      attachInterrupt(PWM_pin[3], PWM_RISING_ISR_3, RISING);  
-
-}
  
  //PWM STUFF has to stay in main page because Arduino...
 
@@ -585,126 +463,4 @@ void falling_rudder() {
   }
   
   //SerialUSB.println(pwm_value[1]);//For testing purposes only
-}
-
-
-//These following interrupt routines can be deleted. 
-
-//interupt service routine called on the rising edge of the pulse
-void PWM_RISING_ISR_0(){
-    //record current time.  this is the rise time
-    pinData[0].riseTime = micros();
-    //calculate the time sice the last falling edge was detected
-    //only count this as a valid rising edge if time difference is more than 1000 microseconds
-    unsigned long time_difference=  pinData[0].riseTime - pinData[0].fallTime;
-
-    if(time_difference > 1000){
-          //if it is a valid rising edge, set the interupt for the falling edge on the same pin
-          attachInterrupt(PWM_pin[0], PWM_FALLING_ISR_0, FALLING);
-    }
-}
-//interrupt service routine called on the falling edge of the pulse
-void PWM_FALLING_ISR_0(){
-    //record current time, this is the fall time of the pulse
-    pinData[0].fallTime = micros();
-
-    //calculate the time difference between the rising and falling edge
-    //only a valid pulse if the time difference is between 400 and 2500 microseconds
-     
-    int time_difference = pinData[0].fallTime - pinData[0].riseTime;   
-    if(time_difference >= 400 && time_difference <= 2500){
-        //if it is a valid pulse, update the pin's last good width
-        pinData[0].lastGoodWidth = time_difference;    
-        //Serial.println(time_difference);
-   }
-   attachInterrupt(PWM_pin[0], PWM_RISING_ISR_0, RISING); 
-}
-
-//interupt service routine called on the rising edge of the pulse
-void PWM_RISING_ISR_1(){
-    //record current time.  this is the rise time
-    pinData[1].riseTime = micros();
-    //calculate the time sice the last falling edge was detected
-    //only count this as a valid rising edge if time difference is more than 1000 microseconds
-    unsigned long time_difference=  pinData[1].riseTime - pinData[1].fallTime;
-
-    if(time_difference > 1000){
-          //if it is a valid rising edge, set the interupt for the falling edge on the same pin
-          attachInterrupt(PWM_pin[1], PWM_FALLING_ISR_1, FALLING);
-    }
-}
-
-//interrupt service routine called on the falling edge of the pulse
-void PWM_FALLING_ISR_1(){
-    //record current time, this is the fall time of the pulse
-    pinData[1].fallTime = micros();
-
-    //calculate the time difference between the rising and falling edge
-    //only a valid pulse if the time difference is between 400 and 2500 microseconds
-     
-    int time_difference = pinData[1].fallTime - pinData[1].riseTime;   
-    if(time_difference >= 400 && time_difference <= 2500){
-        //if it is a valid pulse, update the pin's last good width
-        pinData[1].lastGoodWidth = time_difference;    
-   }
-   attachInterrupt(PWM_pin[1], PWM_RISING_ISR_1, RISING); 
-}
-
-//interupt service routine called on the rising edge of the pulse
-void PWM_RISING_ISR_2(){
-    //record current time.  this is the rise time
-    pinData[2].riseTime = micros();
-    //calculate the time sice the last falling edge was detected
-    //only count this as a valid rising edge if time difference is more than 1000 microseconds
-    unsigned long time_difference=  pinData[2].riseTime - pinData[2].fallTime;
-
-    if(time_difference > 1000){
-          //if it is a valid rising edge, set the interupt for the falling edge on the same pin
-          attachInterrupt(PWM_pin[2], PWM_FALLING_ISR_2, FALLING);
-    }
-}
-
-//interrupt service routine called on the falling edge of the pulse
-void PWM_FALLING_ISR_2(){
-    //record current time, this is the fall time of the pulse
-    pinData[2].fallTime = micros();
-
-    //calculate the time difference between the rising and falling edge
-    //only a valid pulse if the time difference is between 400 and 2500 microseconds
-     
-    int time_difference = pinData[2].fallTime - pinData[2].riseTime;   
-    if(time_difference >= 400 && time_difference <= 2500){
-        //if it is a valid pulse, update the pin's last good width
-        pinData[2].lastGoodWidth = time_difference;    
-   }
-   attachInterrupt(PWM_pin[2], PWM_RISING_ISR_2, RISING); 
-}
-//interupt service routine called on the rising edge of the pulse
-void PWM_RISING_ISR_3(){
-    //record current time.  this is the rise time
-    pinData[3].riseTime = micros();
-    //calculate the time sice the last falling edge was detected
-    //only count this as a valid rising edge if time difference is more than 1000 microseconds
-    unsigned long time_difference=  pinData[3].riseTime - pinData[3].fallTime;
-
-    if(time_difference > 1000){
-          //if it is a valid rising edge, set the interupt for the falling edge on the same pin
-          attachInterrupt(PWM_pin[3], PWM_FALLING_ISR_3, FALLING);
-    }
-}
-
-//interrupt service routine called on the falling edge of the pulse
-void PWM_FALLING_ISR_3(){
-    //record current time, this is the fall time of the pulse
-    pinData[3].fallTime = micros();
-
-    //calculate the time difference between the rising and falling edge
-    //only a valid pulse if the time difference is between 400 and 2500 microseconds
-     
-    int time_difference = pinData[3].fallTime - pinData[3].riseTime;   
-    if(time_difference >= 400 && time_difference <= 2500){
-        //if it is a valid pulse, update the pin's last good width
-        pinData[3].lastGoodWidth = time_difference;    
-   }
-   attachInterrupt(PWM_pin[3], PWM_RISING_ISR_3, RISING); 
 }
