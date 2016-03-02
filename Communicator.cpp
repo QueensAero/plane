999eb121c911149f36a838c224a9a721758f2280
jjollivet <jeremiejollivet@gmail.com>
2016-03-01 20:27:57 -0500

    Removed airspeed, camera servos, PID

--- Communicator.cpp
+++ Communicator.cpp
@@ -13,14 +13,12 @@ Communicator::~Communicator() {}
 void Communicator::initialize() {
 
   //set initial values to 0
-  airspeed = 0;
   altitude = 0;
   roll = 0;
   pitch = 0;
   altitudeAtDrop = 0;
 
   //start without hardware attached
-  cameraAttached = 0;
   dropBayAttached = 0;
   dropBayOpen = 0;
   dropBayDelayTime = 5000;
@@ -39,42 +37,7 @@ void Communicator::recieveCommands() {
     //new command detected, parse and execute
 
     incomingByte = Serial.read();
-    //check to make sure hardware is connected before atempting to write values
-
-    //camera values
-    if (cameraAttached) {
-      if (incomingByte == 's') { //move camera tilt down
-        cameraTiltVal = cameraTiltVal - SERVO_STEP_SIZE;
-        if (cameraTiltVal < TILT_SERVO_MIN)
-          cameraTiltVal = TILT_SERVO_MIN;
-        cameraTiltServo.writeMicroseconds(cameraTiltBaseVal + cameraTiltVal);
-      }
-      if (incomingByte == 'w') { //move camera tilt up
-        cameraTiltVal = cameraTiltVal + SERVO_STEP_SIZE;
-        if (cameraTiltVal > TILT_SERVO_MAX)
-          cameraTiltVal = TILT_SERVO_MAX;
-        cameraTiltServo.writeMicroseconds(cameraTiltBaseVal + cameraTiltVal);
-      }
-      if (incomingByte == 'a') { //move camera pan left
-        cameraPanVal = cameraPanVal - SERVO_STEP_SIZE;
-        if (cameraPanVal < PAN_SERVO_MIN)
-          cameraPanVal = PAN_SERVO_MIN;
-        cameraPanServo.writeMicroseconds(cameraPanBaseVal + cameraPanVal);
-      }
-      if (incomingByte == 'd') { //move camera pan right
-        cameraPanVal = cameraPanVal + SERVO_STEP_SIZE;
-        if (cameraPanVal > PAN_SERVO_MAX)
-          cameraPanVal = PAN_SERVO_MAX;
-        cameraPanServo.writeMicroseconds(cameraPanBaseVal + cameraPanVal);
-      }
-      if (incomingByte == 'x') { //centre camera pan and tilt
-        cameraPanVal = 0;
-        cameraTiltVal = 0;
-        sendMessage(MESSAGE_CAM_RESET);
-        cameraPanServo.writeMicroseconds(cameraPanBaseVal + cameraPanVal);
-        cameraTiltServo.writeMicroseconds(cameraTiltBaseVal + cameraTiltVal);
-      }
-    }
+    //check to make sure hardware is connected before atempting to write value
 
     //drop bay
     if (dropBayAttached) {
@@ -97,39 +60,16 @@ void Communicator::recieveCommands() {
     if (incomingByte == 'q') {  //RESTART FUNCTION.
       while (sequence_step > 0) sendData();  //flush current data packets
       restart = true;
-      cameraPanVal = 0;
-      cameraTiltVal = 0;
-      cameraTiltServo.writeMicroseconds(cameraTiltBaseVal + cameraTiltVal);
-      cameraPanServo.writeMicroseconds(cameraTiltBaseVal + cameraPanVal);
       dropVal = DROP_BAY_CLOSED;
       dropServo.writeMicroseconds(dropVal);
     }
     if (incomingByte == '$'){
       if(millis() < 10000){
-          calibration_flag = true;      
-          initalizeCalibration();  
+          calibration_flag = true;        
       }
     }
     
   }
-}
-void Communicator::attachCamera(int _cameraPanPin, int _cameraTiltPin) {
-  cameraAttached = 1;
-
-  cameraPanPin = _cameraPanPin;
-  cameraTiltPin = _cameraTiltPin;
-
-  cameraTiltVal = 0;
-  cameraPanVal = 0;
-  
-  cameraTiltBaseVal = CAMERA_TILT_START_VAL;
-  cameraPanBaseVal = CAMERA_PAN_START_VAL;
-
-  cameraTiltServo.attach(cameraTiltPin);
-  cameraPanServo.attach(cameraPanPin);
-
-  cameraTiltServo.writeMicroseconds(cameraTiltBaseVal + cameraTiltVal);
-  cameraPanServo.writeMicroseconds(cameraTiltBaseVal + cameraPanVal);
 }
 
 void Communicator::attachDropBay(int _dropServoPin) {
@@ -140,13 +80,6 @@ void Communicator::attachDropBay(int _dropServoPin) {
   dropServo.writeMicroseconds(dropVal);
 }
 
-
-int Communicator::getCameraTiltPin () {
-  return cameraTiltPin;
-}
-int Communicator::getCameraPanPin () {
-  return cameraPanPin;
-}
 int Communicator::getDropPin () {
   return dropServoPin;
 }
@@ -168,12 +101,6 @@ void Communicator:: sendData() {
     Serial.print('%');
     Serial.print(altitude);
   }
-  if (sequence_step == 3) {
-    Serial.print('%');
-    Serial.print(airspeed);
-    Serial.print('&');
-    sequence_step = -1;
-  }
   sequence_step++;
 }
 void Communicator::sendMessage(char message) {
@@ -184,19 +111,6 @@ void Communicator::sendMessage(char message) {
   Serial.print("*");
   Serial.print(message);
   Serial.print("&");
-}
-
-void Communicator::initalizeCalibration(){
-  
-  cameraTiltBaseVal = 1500;
-  cameraPanBaseVal = 1500;
-  LeftAileronVal_cal = 1500;
-  RightAileronVal_cal = 1500;
-  ElevatorVal_cal = 1500;  
-  
-  cameraTiltServo.writeMicroseconds(cameraTiltBaseVal);
-  cameraPanServo.writeMicroseconds(cameraPanBaseVal);
-  
 }
 
 void Communicator::calibrate(){
@@ -232,9 +146,6 @@ void Communicator::calibrate(){
         if (incoming_byte == '*') {
           // termanating character found, message received
           waiting_for_message = false;
-          
-          //determine SERVO_target number by converting ASCII character to number
-          int SERVO_target_number = serial_buffer[0] - 48;
 
           //transfer value part of message into new char array
           int message_size = serial_index - 3;   //message starts on character # 3 and serial_index has been incrememted by 1 at this point
@@ -248,37 +159,7 @@ void Communicator::calibrate(){
           int val = atoi(message);
           if (val > 2000) val = 2000;
           if (val < 0) val = 0;
-  
-          //check to make sure that the values are in the correct range
-          if (SERVO_target_number > 7) SERVO_target_number = 7;
-          if (SERVO_target_number < 0) SERVO_target_number = 0;
-  
-          //Convert val to match servo range
-          int servoVal;
           
-          Serial.print(SERVO_target_number);
-          Serial.print("/");
-          Serial.print(val);
-          Serial.print("*");
-  
-          if (SERVO_target_number == 0){ 
-            cameraTiltBaseVal = val;
-            cameraTiltServo.writeMicroseconds(cameraTiltBaseVal);
-          }
-          if (SERVO_target_number == 1){ 
-            cameraPanBaseVal = val;
-            cameraPanServo.writeMicroseconds(cameraPanBaseVal);
-          }
-          if (SERVO_target_number == 2){ 
-            LeftAileronVal_cal = val;
-          }
-          if (SERVO_target_number == 3){ 
-            RightAileronVal_cal = val;
-          }
-          if (SERVO_target_number == 4){
-            ElevatorVal_cal = val;
-          }
-  
           Serial.flush();
           serial_index = 0; //start over for next message
           free(message);
