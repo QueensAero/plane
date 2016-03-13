8990791862407d402fe80b5bbf17b786a19ef1ae
RyanDowlingQueens <rdowling7@gmail.com>
2016-03-13 17:21:16 -0400

    Changed GPS & Comm interaction
    Significantly altered from original library method. See comments near top for details.  Changed how it's declared as well (have empty constructor). Highlight is moving all serial communication and receiving of raw NMEA strings into the communicator class

--- Adafruit_GPS.cpp
+++ Adafruit_GPS.cpp
@@ -9,25 +9,38 @@ Written by Limor Fried/Ladyada for Adafruit Industries.
 BSD license, check license.txt for more information
 All text above must be included in any redistribution
 ****************************************/
-#ifdef __AVR__
-  // Only include software serial on AVR platforms (i.e. not on Due).
-  #include <SoftwareSerial.h>
-#endif
+
 #include "Adafruit_GPS.h"
 
-// how long are max NMEA lines to parse?
-#define MAXLINELENGTH 120
+/*General Operation Notes (Ryan Dowling)
 
-// we double buffer: read one line in and leave one for the main program
-volatile char line1[MAXLINELENGTH];
-volatile char line2[MAXLINELENGTH];
-// our index into filling the current line
-volatile uint8_t lineidx=0;
-// pointers to the double buffers
-volatile char *currentline;
-volatile char *lastline;
-volatile boolean recvdflag;
-volatile boolean inStandbyMode;
+This is significantly changed from library implementation.  Summary of changes:
+- Moved all serial communication with GPS into communicator.cpp, for more flexibility 
+- Removed parsing of strings other than GPRMC (since we only use GPRMC)
+- Altered parsing function - see comment in it about checksum now starting from buffer index 1 (instead 2)
+- The raw NMEA string is now housed in communicator
+- This class essentially only functions by being passed a raw NMEA string to be parsed. It takes care of checksum. 
+
+*/
+
+Adafruit_GPS::Adafruit_GPS(){
+	//empty constructor (all initialization done in the init function)
+}
+
+
+// Initialization code
+void Adafruit_GPS::init() {
+
+	//initialize state variables
+	hour = minute = seconds = year = month = day = fixquality = satellites = 0; // uint8_t
+	lat = lon = mag = 0; // char
+	fix = false; // boolean
+	milliseconds = 0; // uint16_t
+	latitude = longitude = geoidheight = altitude =	speed = angle = magvariation = HDOP = 0.0; // float
+  lat = lon = '0';
+
+}
+
 
 
 boolean Adafruit_GPS::parse(char *nmea) {
@@ -39,121 +52,25 @@ boolean Adafruit_GPS::parse(char *nmea) {
     sum += parseHex(nmea[strlen(nmea)-2]);
     
     // check checksum 
-    for (uint8_t i=2; i < (strlen(nmea)-4); i++) {
+    //NOTE the starting at i=1!! based on how their code worked, the first the string actually begin as:  s[0] = '\n', s[1] = $, s[2] = 'G'  ie. the checksum characters start at i=2 index, not i=1 as would be expected
+    //In the original library code this for loop began at i=2 to account for:they set the index back to 0, then immidately 'added' the current characer (the newline) at index 0, then the next string began with $ at s[1]
+    //Be wary of how the string is sent!
+    for (uint8_t i=1; i < (strlen(nmea)-4); i++) {  
       sum ^= nmea[i];
     }
     if (sum != 0) {
-      // bad checksum :(
+      // bad checksum
+      //SerialUSB.println("Bad checksum");
       return false;
     }
   }
+
+
   int32_t degree;
   long minutes;
   char degreebuff[10];
-  // look for a few common sentences
-  if (strstr(nmea, "$GPGGA")) {
-    // found GGA
-    char *p = nmea;
-    // get time
-    p = strchr(p, ',')+1;
-    float timef = atof(p);
-    uint32_t time = timef;
-    hour = time / 10000;
-    minute = (time % 10000) / 100;
-    seconds = (time % 100);
 
-    milliseconds = fmod(timef, 1.0) * 1000;
-
-    // parse out latitude
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      strncpy(degreebuff, p, 2);
-      p += 2;
-      degreebuff[2] = '\0';
-      degree = atol(degreebuff) * 10000000;
-      strncpy(degreebuff, p, 2); // minutes
-      p += 3; // skip decimal point
-      strncpy(degreebuff + 2, p, 4);
-      degreebuff[6] = '\0';
-      minutes = 50 * atol(degreebuff) / 3;
-      latitude_fixed = degree + minutes;
-      latitude = degree / 100000 + minutes * 0.000006F;
-      latitudeDegrees = (latitude-100*int(latitude/100))/60.0;
-      latitudeDegrees += int(latitude/100);
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      if (p[0] == 'S') latitudeDegrees *= -1.0;
-      if (p[0] == 'N') lat = 'N';
-      else if (p[0] == 'S') lat = 'S';
-      else if (p[0] == ',') lat = 0;
-      else return false;
-    }
-    
-    // parse out longitude
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      strncpy(degreebuff, p, 3);
-      p += 3;
-      degreebuff[3] = '\0';
-      degree = atol(degreebuff) * 10000000;
-      strncpy(degreebuff, p, 2); // minutes
-      p += 3; // skip decimal point
-      strncpy(degreebuff + 2, p, 4);
-      degreebuff[6] = '\0';
-      minutes = 50 * atol(degreebuff) / 3;
-      longitude_fixed = degree + minutes;
-      longitude = degree / 100000 + minutes * 0.000006F;
-      longitudeDegrees = (longitude-100*int(longitude/100))/60.0;
-      longitudeDegrees += int(longitude/100);
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      if (p[0] == 'W') longitudeDegrees *= -1.0;
-      if (p[0] == 'W') lon = 'W';
-      else if (p[0] == 'E') lon = 'E';
-      else if (p[0] == ',') lon = 0;
-      else return false;
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      fixquality = atoi(p);
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      satellites = atoi(p);
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      HDOP = atof(p);
-    }
-    
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      altitude = atof(p);
-    }
-    
-    p = strchr(p, ',')+1;
-    p = strchr(p, ',')+1;
-    if (',' != *p)
-    {
-      geoidheight = atof(p);
-    }
-    return true;
-  }
+  //GPRMC parsing
   if (strstr(nmea, "$GPRMC")) {
    // found RMC
     char *p = nmea;
@@ -165,11 +82,9 @@ boolean Adafruit_GPS::parse(char *nmea) {
     hour = time / 10000;
     minute = (time % 10000) / 100;
     seconds = (time % 100);
-
     milliseconds = fmod(timef, 1.0) * 1000;
 
     p = strchr(p, ',')+1;
-    // Serial.println(p);
     if (p[0] == 'A') 
       fix = true;
     else if (p[0] == 'V')
@@ -179,7 +94,7 @@ boolean Adafruit_GPS::parse(char *nmea) {
 
     // parse out latitude
     p = strchr(p, ',')+1;
-    if (',' != *p)
+    if (',' != *p)  //double comma would indicate we don't have a lattitude 
     {
       strncpy(degreebuff, p, 2);
       p += 2;
@@ -190,6 +105,8 @@ boolean Adafruit_GPS::parse(char *nmea) {
       strncpy(degreebuff + 2, p, 4);
       degreebuff[6] = '\0';
       long minutes = 50 * atol(degreebuff) / 3;
+
+    //a coupel ways of representing lattitude - pure degrees, degreeminutes and stuff.  Need to decide which format. Same below with Longitude
       latitude_fixed = degree + minutes;
       latitude = degree / 100000 + minutes * 0.000006F;
       latitudeDegrees = (latitude-100*int(latitude/100))/60.0;
@@ -258,131 +175,12 @@ boolean Adafruit_GPS::parse(char *nmea) {
     }
     // we dont parse the remaining, yet!
     return true;
-  }
+  }  
 
   return false;
 }
 
-char Adafruit_GPS::read(void) {
-  char c = 0;
-  
-  if (paused) return c;
 
-#ifdef __AVR__
-  if(gpsSwSerial) {
-    if(!gpsSwSerial->available()) return c;
-    c = gpsSwSerial->read();
-  } else 
-#endif
-  {
-    if(!gpsHwSerial->available()) return c;
-    c = gpsHwSerial->read();
-  }
-
-  //Serial.print(c);
-
-//  if (c == '$') {         //please don't eat the dollar sign - rdl 9/15/14
-//    currentline[lineidx] = 0;
-//    lineidx = 0;
-//  }
-  if (c == '\n') {
-    currentline[lineidx] = 0;
-
-    if (currentline == line1) {
-      currentline = line2;
-      lastline = line1;
-    } else {
-      currentline = line1;
-      lastline = line2;
-    }
-
-    //Serial.println("----");
-    //Serial.println((char *)lastline);
-    //Serial.println("----");
-    lineidx = 0;
-    recvdflag = true;
-  }
-
-  currentline[lineidx++] = c;
-  if (lineidx >= MAXLINELENGTH)
-    lineidx = MAXLINELENGTH-1;
-
-  return c;
-}
-
-#ifdef __AVR__
-// Constructor when using SoftwareSerial or NewSoftSerial
-#if ARDUINO >= 100
-Adafruit_GPS::Adafruit_GPS(SoftwareSerial *ser)
-#else
-Adafruit_GPS::Adafruit_GPS(NewSoftSerial *ser) 
-#endif
-{
-  common_init();     // Set everything to common state, then...
-  gpsSwSerial = ser; // ...override gpsSwSerial with value passed.
-}
-#endif
-
-// Constructor when using HardwareSerial
-Adafruit_GPS::Adafruit_GPS(HardwareSerial *ser) {
-  common_init();  // Set everything to common state, then...
-  gpsHwSerial = ser; // ...override gpsHwSerial with value passed.
-}
-
-// Initialization code used by all constructor types
-void Adafruit_GPS::common_init(void) {
-#ifdef __AVR__
-  gpsSwSerial = NULL; // Set both to NULL, then override correct
-#endif
-  gpsHwSerial = NULL; // port pointer in corresponding constructor
-  recvdflag   = false;
-  paused      = false;
-  lineidx     = 0;
-  currentline = line1;
-  lastline    = line2;
-
-  hour = minute = seconds = year = month = day =
-    fixquality = satellites = 0; // uint8_t
-  lat = lon = mag = 0; // char
-  fix = false; // boolean
-  milliseconds = 0; // uint16_t
-  latitude = longitude = geoidheight = altitude =
-    speed = angle = magvariation = HDOP = 0.0; // float
-}
-
-void Adafruit_GPS::begin(uint16_t baud)
-{
-#ifdef __AVR__
-  if(gpsSwSerial) 
-    gpsSwSerial->begin(baud);
-  else 
-    gpsHwSerial->begin(baud);
-#endif
-
-  delay(10);
-}
-
-void Adafruit_GPS::sendCommand(const char *str) {
-#ifdef __AVR__
-  if(gpsSwSerial) 
-    gpsSwSerial->println(str);
-  else    
-#endif
-    gpsHwSerial->println(str);
-}
-
-boolean Adafruit_GPS::newNMEAreceived(void) {
-  return recvdflag;
-}
-
-void Adafruit_GPS::pause(boolean p) {
-  paused = p;
-}
-
-char *Adafruit_GPS::lastNMEA(void) {
-  recvdflag = false;
-  return (char *)lastline;
-}
 
 // read a Hex value and return the decimal equivalent
 uint8_t Adafruit_GPS::parseHex(char c) {
@@ -397,6 +195,26 @@ uint8_t Adafruit_GPS::parseHex(char c) {
     // if (c > 'F')
     return 0;
 }
+
+
+
+
+/* The following will no longer work since I've moved the serial communication to the communicator class for greater flexibility
+
+// how long are max NMEA lines to parse?
+#define MAXLINELENGTH 120
+
+// we double buffer: read one line in and leave one for the main program
+volatile char line1[MAXLINELENGTH];
+volatile char line2[MAXLINELENGTH];
+// our index into filling the current line
+volatile uint8_t lineidx=0;
+// pointers to the double buffers
+volatile char *currentline;
+volatile char *lastline;
+volatile boolean recvdflag;
+volatile boolean inStandbyMode;
+
 
 boolean Adafruit_GPS::waitForSentence(const char *wait4me, uint8_t max) {
   char str[20];
@@ -498,3 +316,4 @@ boolean Adafruit_GPS::wakeup(void) {
       return false;  // Returns false if not in standby mode, nothing to wakeup
   }
 }
+*/
