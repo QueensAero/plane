a82fe92bae83bc8c1eee1df94ca9153f59549db0
RyanDowlingQueens <rdowling7@gmail.com>
2016-04-03 23:31:55 -0400

    PWM input, Comm send format
    added PWM pin definitions

--- plane.h
+++ plane.h
@@ -1,21 +1,32 @@
-//DX6 RC receiver
-#define ELEVATOR_INPUT_PIN 7
-#define RUDDER_INPUT_PIN 6
+//OUTPUT pins (as labelled on PCB)
+#define RUDDER_OUT_PIN 2
+#define L_TAIL_OUT_PIN 3
+#define R_TAIL_OUT_PIN 4
+#define R_AILERON_OUT_PIN 5
+#define L_AILERON_OUT_PIN 6
+#define SPARE_OUT_PIN 13
+//#define DROP_OUT_PIN 10  //note this is done by communicator class
 
-//system timing variables in microseconds
-#define SLOW_LOOP_TIME 250 //100 miliseconds
-#define MEDIUM_LOOP_TIME 50 //50 miliseconds
-#define FAST_LOOP_TIME 1  //1 milisecond
-#define LONG_LOOP_TIME 500 //0.5 seconds
 
-//define Demixing & servo pin constants 
-#define TAIL_WHEEL_PIN 12
-#define TAIL_WHEEL_NEUTRAL 1500  //NEED TO CHECK!
+//INPUT pins (as labelled on PCB). These labels are misleading/innaccurate
+#define PID_MODE_INPUT 7
+#define PITCH_INPUT 8
+#define ROLL_INPUT 9
+#define FLAP_MODE_INPUT 11
+#define YAW_INPUT 12
+
+ 
+//define Demixing constants 
 #define FLIP_LEFT_SIGNAL false
 #define FLIP_RIGHT_SIGNAL false
 #define LEFT_SIGNAL_OFFSET 100
 #define RIGHT_SIGNAL_OFFSET 100
 
+//system timing variables in microseconds
+#define SLOW_LOOP_TIME 250    //Xbee send packets of data 
+#define MEDIUM_LOOP_TIME 50   //servo updating
+#define FAST_LOOP_TIME 1  	  //MPU updating, if PID's then compute new servo values
+#define LONG_LOOP_TIME 500 	  //LED blinking
 
 //hardware declerations
 #define STATUS_LED_PIN 50
