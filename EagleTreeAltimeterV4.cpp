7c32d5861835d2c6cf88122401b1ee568d1cae3b
RyanDowlingQueens <rdowling7@gmail.com>
2016-09-23 17:09:40 -0400

    2016 Competition Code

--- EagleTreeAltimeterV4.cpp
+++ EagleTreeAltimeterV4.cpp
@@ -3,6 +3,8 @@
 //SDA: pin A4 / yellow wire
 //SCL pin A5 / brown wire
 
+#define TIMEOUT_MS 30
+
 EagleTreeAltimeterV4::EagleTreeAltimeterV4() {
 
 }
@@ -22,26 +24,57 @@ void EagleTreeAltimeterV4::zero () {
 
 int EagleTreeAltimeterV4::readRawData() {
   int data;
-  byte a, b;
   //send "read" command to sensor
+
+  //uint8_t values[2];
+  byte values[2];
+
+  int numReceived = I2Cdev::readBytes(altimeterAddress, 0x07, 2, values, TIMEOUT_MS);  //30 ms timeout (test this)
+
+
+  if(numReceived == -1)       //I2C timeout, value received no good
+  {    
+        //temporary: (somehow this should eventually be moved to the Communicator class
+        Serial3.print("*tee");
+        return -1;
+  }
+  
+  /*
+  //Replaced with I2Cdev version which should avoid timeout
   Wire.beginTransmission(altimeterAddress);
   Wire.write(0x07);
   Wire.endTransmission();
+
+  
   //request 2 bytes
   Wire.beginTransmission(altimeterAddress);
   Wire.requestFrom(altimeterAddress, 2);
-  while(Wire.available()<2);
-  a = Wire.read();
-  b = Wire.read();
+  SerialUSB.println("second bit");
+
+  
+  while(Wire.available() < 2)
+  {
+      SerialUSB.println(Wire.available());
+      delay(100);
+  }
+  values[0] = Wire.read();
+  values[1] = Wire.read();
+  SerialUSB.println("third bit");
   Wire.endTransmission();
+  */
+
+
   //combine the bytes to an integer
-  data = (a) | (b << 8);
+  data = (values[0]) | (values[1] << 8);
   return data;
 } 
 
 double EagleTreeAltimeterV4::readAltitude() {
   int data = readRawData();
-  filter->addValue(data);
+
+  if(data != -1)   //only add the data to filter if it's valid
+    filter->addValue(data);
+  
   return (filter->getCurrentValue() - zeroVal) * DEC_TO_FEET;
 }
 
