a9f4bb29e64a7fe728e3956d24f76ea1908789b5
vacer25 <vacer25@gmail.com>
2017-01-23 21:36:12 -0500

    Update Communicator.cpp
    Added auto targeter self-test mode.

--- Communicator.cpp
+++ Communicator.cpp
@@ -11,21 +11,6 @@
 #include <Servo.h>
 #include "Targeter.h"
 
-// During testing we might want to send over USB to computer. Instead of commenting out a lot of  'SerialUSB.print(...)' statements we can define a macro as below
-// If the line directly below is NOT commented out, then DEGUB_PRINT(...) will send to computer. If it is commented out, the macro DEBUG_PRINT/LN will be empty and
-// the compiler will optimize it out of the code automatically
-#define DEBUG_COMMUNICATOR
-
-#ifdef DEBUG_COMMUNICATOR
-#define DEBUG_SERIAL Serial
-#define DEBUG_PRINT(x) DEBUG_SERIAL.print(x)
-#define DEBUG_PRINTLN(x) DEBUG_SERIAL.println(x)
-#define DEBUG_BEGIN(x) DEBUG_SERIAL.begin(x)
-#else
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTLN(x)
-#define DEBUG_BEGIN(x) 
-#endif
 
 Adafruit_GPS GPS;
 Targeter targeter;
@@ -33,6 +18,25 @@ Targeter targeter;
 //Constructor
 Communicator::Communicator() {}
 Communicator::~Communicator() {}
+
+// -------------------------------------------- DEBUG --------------------------------------------
+
+#ifdef Targeter_Test
+
+//static float GPSLatitudes[] = {4413.546, 4413.574, 4413.610, 4413.636, 4413.660};
+//static float GPSLongitudes[] = { -7629.504, -7629.507, -7629.509, -7629.513, -7629.514};
+
+static float GPSLatitudes[] = {4413.546, 4413.574, 4413.610, 4413.636, 4413.688};
+static float GPSLongitudes[] = { -7629.504, -7629.507, -7629.509, -7629.513, -7629.518};
+// Points start at south end of bioscience complex and move North along Arch street
+
+static float altitudes[] = {100, 100, 100, 100, 100};
+static float velocities[] = {10, 20, 30, 40, 50};
+static float headings[] = {360, 360, 360, 360, 360};
+
+#define numberOfTargeterDataPoint sizeof(GPSLatitudes) / sizeof(GPSLatitudes[0])
+
+#endif
 
 // Function called in void setup() that instantiates all the variables, attaches pins, ect
 // This funciton needs to be called before anything else will work
@@ -59,7 +63,7 @@ void Communicator::initialize() {
   dropServo.writeMicroseconds(dropBayServoPos);
 
   int maxTries = 3, numTries = 0;
-  while(!initXBee() && ++numTries <= maxTries);  //Keep trying to put into transparent mode until failure
+  while (!initXBee() && ++numTries <= maxTries); //Keep trying to put into transparent mode until failure
 
 
   targeter.setTargetData(targetLatitude, targetLongitude, targetAltitude);
@@ -74,17 +78,17 @@ bool Communicator::initXBee()
 {
   // Initialize serial commuication to Xbee.
   XBEE_SERIAL.begin(XBEE_BAUD);  //this is to Xbee
-  while(!XBEE_SERIAL);  //wait until it's ready
+  while (!XBEE_SERIAL); //wait until it's ready
 
 
   //Put into command mode, switch to transparent (not API) mode, then exit command mode
-  if(!sendCmdAndWaitForOK("+++"))
-  {    
+  if (!sendCmdAndWaitForOK("+++"))
+  {
     DEBUG_PRINTLN("Failed on '+++'");
     return false;
   }
 
-  if(!sendCmdAndWaitForOK("ATAP0\r"))
+  if (!sendCmdAndWaitForOK("ATAP0\r"))
   {
     DEBUG_PRINTLN("Failed on 'ATAP0'");
     return false;
@@ -94,10 +98,10 @@ bool Communicator::initXBee()
   //sendCmdAndWaitForOK("ATID\r");  //To check network ID
 
 
-  if(!sendCmdAndWaitForOK("ATCN\r"))
+  if (!sendCmdAndWaitForOK("ATCN\r"))
   {
     DEBUG_PRINTLN("Failed on 'ATCN'");
-    return false;  
+    return false;
   }
 
 
@@ -108,7 +112,7 @@ bool Communicator::initXBee()
 bool Communicator::sendCmdAndWaitForOK(String cmd, int timeout)
 {
   //Flush input
-  while(XBEE_SERIAL.read() != -1);
+  while (XBEE_SERIAL.read() != -1);
 
   //Send Command
   XBEE_SERIAL.print(cmd);
@@ -121,10 +125,10 @@ bool Communicator::sendCmdAndWaitForOK(String cmd, int timeout)
   DEBUG_PRINT("Response: ");
   DEBUG_PRINTLN(response);
 
-  if(response.endsWith("OK"))
+  if (response.endsWith("OK"))
     return true;
   else
-    return false;  
+    return false;
 }
 
 
@@ -132,15 +136,52 @@ bool Communicator::sendCmdAndWaitForOK(String cmd, int timeout)
 //src == 1 corresponds to the automatic drop function.
 //state == 0 closes drop bay. state == 1 opens drop bay.
 void Communicator::dropNow(int src, int state) {
-  if (src == 1 && autoDrop == false && state == 1) //AutoDrop Protection
+
+#ifdef Targeter_Test
+  DEBUG_PRINTLN("");
+  DEBUG_PRINTLN("[AUTO DROP]");
+  DEBUG_PRINTLN("");
+#endif
+
+  if (src == 1 && autoDrop == false && state == 1) { //AutoDrop Protection
+#ifdef Targeter_Test
+    DEBUG_PRINT("Did not drop (src = ");
+    DEBUG_PRINT(src);
+    DEBUG_PRINT("   autoDrop = ");
+    DEBUG_PRINT(autoDrop);
+    DEBUG_PRINT("   state = ");
+    DEBUG_PRINT(state);
+    DEBUG_PRINTLN(")");
+#endif
     return;
+  }
 
   //Open/Close Drop Bay
   if (state == 0)  {
+#ifdef Targeter_Test
+    DEBUG_PRINT("Closed bay door (src = ");
+    DEBUG_PRINT(src);
+    DEBUG_PRINT("   autoDrop = ");
+    DEBUG_PRINT(autoDrop);
+    DEBUG_PRINT("   state = ");
+    DEBUG_PRINT(state);
+    DEBUG_PRINTLN(")");
+#endif
+    digitalWrite(STATUS_LED_PIN, LOW);
     dropBayServoPos = DROP_BAY_CLOSED;
     sendMessage(MESSAGE_DROP_CLOSE);
   }
   else {
+#ifdef Targeter_Test
+    DEBUG_PRINT("Opened bay door (src = ");
+    DEBUG_PRINT(src);
+    DEBUG_PRINT("   autoDrop = ");
+    DEBUG_PRINT(autoDrop);
+    DEBUG_PRINT("   state = ");
+    DEBUG_PRINT(state);
+    DEBUG_PRINTLN(")");
+#endif
+    digitalWrite(STATUS_LED_PIN, HIGH);
     dropBayServoPos = DROP_BAY_OPEN;
     altitudeAtDrop = altitude;
     timeAtDrop = millis();
@@ -159,6 +200,11 @@ void Communicator::checkToCloseDropBay() {
     unsigned long currentMillis = millis();
 
     if (currentMillis - timeAtDrop >= closeDropBayTimeout && currentMillis - timeAtDrop < closeDropBayTimeout + 10000) {
+#ifdef Targeter_Test
+      DEBUG_PRINT("Auto closing bay door (time passed = ");
+      DEBUG_PRINT((currentMillis - timeAtDrop));
+      DEBUG_PRINTLN(")");
+#endif
       dropNow(0, 0);
     }
 
@@ -168,14 +214,47 @@ void Communicator::checkToCloseDropBay() {
 
 void Communicator::recalculateTargettingNow(boolean withNewData) {
 
+#ifdef Targeter_Test
+
+  DEBUG_PRINTLN("");
+  DEBUG_PRINTLN("[RECALCULATE TARGETING]");
+  DEBUG_PRINTLN("");
+
+  currentTargeterDataPoint++;
+  if (currentTargeterDataPoint == numberOfTargeterDataPoint) {
+    currentTargeterDataPoint = 0;
+  }
+  DEBUG_PRINT("Moving on to next test data point #");
+  DEBUG_PRINTLN(currentTargeterDataPoint);
+
+#endif
+
   bool isReadyToDrop = false;
 
   if (withNewData) {
+#ifdef Targeter_Test
+
+    //DEBUG_PRINT("Current test data point =");
+    //DEBUG_PRINTLN(currentTargeterDataPoint);
+
+    isReadyToDrop = targeter.setCurrentData(GPSLatitudes[currentTargeterDataPoint], GPSLongitudes[currentTargeterDataPoint], altitudes[currentTargeterDataPoint], velocities[currentTargeterDataPoint], headings[currentTargeterDataPoint], millis());
+
+    if (dropBayServoPos == DROP_BAY_OPEN) {
+      isReadyToDrop = false;
+    }
+
+#else
     isReadyToDrop = targeter.setCurrentData(GPS.latitude, GPS.longitude, altitude, GPS.speed, GPS.angle, millis());
+#endif
   }
   else {
     isReadyToDrop = targeter.recalculate();
   }
+
+#ifdef Targeter_Test
+  DEBUG_PRINT("Ready to drop = ");
+  DEBUG_PRINTLN(isReadyToDrop);
+#endif
 
   if (isReadyToDrop && autoDrop) {
     dropNow(1, 1);
@@ -199,18 +278,18 @@ void Communicator::recieveCommands() {
       dropNow(0, 1);
     else if (incomingByte == INCOME_DROP_CLOSE)
       dropNow(0, 0);
-    
 
-    //Auto drop (Toggle)
-    if (incomingByte == INCOME_AUTO) {
-      if (autoDrop == false) {
-        autoDrop = true;
-        sendMessage(MESSAGE_AUTO_ON);
-      }
-      else {
-        autoDrop = false;
-        sendMessage(MESSAGE_AUTO_OFF);
-      }
+
+    // Turn ON auto drop
+    if (incomingByte == INCOME_AUTO_ON) {
+      autoDrop = true;
+      sendMessage(MESSAGE_AUTO_ON);
+    }
+
+    // Turn OFF auto drop
+    if (incomingByte == INCOME_AUTO_OFF) {
+      autoDrop = false;
+      sendMessage(MESSAGE_AUTO_OFF);
     }
 
     // Reset
@@ -222,7 +301,7 @@ void Communicator::recieveCommands() {
     if (incomingByte == INCOME_RESTART) {  //RESTART FUNCTION.
       sendData();  //Flush current data packets
       restart = true;
-      dropNow(0,0);  //close drop bay
+      dropNow(0, 0); //close drop bay
     }
 
     if (incomingByte == INCOME_DROP_ALT) {  //SEND ALTITUDE_AT_DROP
@@ -240,7 +319,8 @@ void Communicator::getSerialDataFromGPS() {
   while (GPS_SERIAL.available()) {
 
     nmeaBuf[nmeaBufInd] = GPS_SERIAL.read();
-    //DEBUG_PRINT(nmeaBuf[nmeaBufInd]);
+
+    DEBUG_PRINT(nmeaBuf[nmeaBufInd]);
     if (nmeaBuf[nmeaBufInd++] == '\n') { // Increment index after checking if current character signifies the end of a string
       nmeaBuf[nmeaBufInd - 1] = '\0'; // Add null terminating character (note: -1 is because nmeaBufInd is incremented in if statement)
       newParsedData = GPS.parse(nmeaBuf); 	// This parses the string, and updates the values of GPS.lattitude, GPS.longitude etc.
@@ -260,6 +340,8 @@ void Communicator::getSerialDataFromGPS() {
 }
 
 void Communicator::setupGPS() {
+
+  DEBUG_PRINTLN("GPS Initilization...");
 
   // Initialize the variables in GPS class object
   GPS.init();
@@ -327,16 +409,16 @@ void Communicator::sendData() {
   sendUint8_t(GPS.seconds);
   XBEE_SERIAL.print("ee");
 
-/*
-  DEBUG_PRINT("Message: ");
-  DEBUG_PRINT("\n");
-  DEBUG_PRINTLN(altitude);
-  DEBUG_PRINTLN(GPS.speed);
-  DEBUG_PRINTLN(GPS.latitude);
-  DEBUG_PRINTLN(GPS.longitude);
-  DEBUG_PRINTLN(GPS.milliseconds);
-  DEBUG_PRINTLN(GPS.seconds);
-*/
+  /*
+    DEBUG_PRINT("Message: ");
+    DEBUG_PRINT("\n");
+    DEBUG_PRINTLN(altitude);
+    DEBUG_PRINTLN(GPS.speed);
+    DEBUG_PRINTLN(GPS.latitude);
+    DEBUG_PRINTLN(GPS.longitude);
+    DEBUG_PRINTLN(GPS.milliseconds);
+    DEBUG_PRINTLN(GPS.seconds);
+  */
 }
 
 void Communicator::sendMessage(char message) {
@@ -404,19 +486,19 @@ boolean Communicator::delayUntilSerialData(unsigned long ms_timeout) {
 
 
 /*
- * 
- * XBEE_SERIAL.print("+++");  //enter command mode
- * //TODO - check 'OK' was returned
- * delay(1500);
- * XBEE_SERIAL.print("ATAP0);  //set to transparent mode (no longer API)
- * //TODO - check 'OK' was returned
- * delay(1500);
- * XBEE_SERIAL.print("ATCN");  //exit command mode
- * //TODO - check 'OK' was returned
 
- * 
- * 
- */
+   XBEE_SERIAL.print("+++");  //enter command mode
+   //TODO - check 'OK' was returned
+   delay(1500);
+   XBEE_SERIAL.print("ATAP0);  //set to transparent mode (no longer API)
+   //TODO - check 'OK' was returned
+   delay(1500);
+   XBEE_SERIAL.print("ATCN");  //exit command mode
+   //TODO - check 'OK' was returned
+
+
+
+*/
 /*  No longer used.  Leave in case needed for later years
   void Communicator::calibrate(){
   //set inital calibration values
